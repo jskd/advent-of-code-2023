e8b19950a4906ccd57eeac26af4e905d6e7176ec
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-01 23:57:41 +0100

    refacto

--- src/day-01/day-1-part-2.ts
+++ src/day-01/day-1-part-2.ts
@@ -1,47 +1,38 @@
-
-const reverseStr = (str : string) => str.split("").reverse().join("");
-
-const literalDigitMap: Map<string, string> = new Map([
-    ["one", "1"], 
-    ["two", "2"],
-    ["three", "3"],
-    ["four", "4"],
-    ["five", "5"],
-    ["six", "6"],
-    ["seven","7"],
-    ["eight", "8"],
-    ["nine", "9"],
-]);
-
 export abstract class Day1Part2 {
 
-    static replaceFirstLiteralByDigit(input: string) {
-        const literalTokens = [...literalDigitMap.keys()].join('|');
-        const regex = new RegExp(`(${literalTokens})`);
-        return input.replace(regex, (match) => {
-            return literalDigitMap.get(match) ?? ""
-        });
+    static readonly literalDigitMap: Map<string, string> = new Map([
+        ["one", "1"], 
+        ["two", "2"],
+        ["three", "3"],
+        ["four", "4"],
+        ["five", "5"],
+        ["six", "6"],
+        ["seven","7"],
+        ["eight", "8"],
+        ["nine", "9"],
+    ]);
+    
+    static readonly literalTokens = [...this.literalDigitMap.keys()].join('|');
+    
+    static replaceFirstLiteralByDigit(input: string): string {
+        const lazy = new RegExp(`(.*?)(${this.literalTokens})`);
+        return input.replace(lazy, (_, p1, p2) => p1 + (this.literalDigitMap.get(p2) ?? ""));
     }
-
-    static replaceLastLiteralByDigit(input: string) {
-        const literalTokens = reverseStr([...literalDigitMap.keys()].join('|'));
-        const regex = new RegExp(`(${literalTokens})`);
-        return reverseStr(input).replace(regex, (match) => {
-            return literalDigitMap.get(reverseStr(match)) ?? ""
-        });
+    
+    static replaceLastLiteralByDigit(input: string): string {
+        const greedy = new RegExp(`(.*)(${this.literalTokens})`);
+        return input.replace(greedy, (_, p1, p2) => p1 + (this.literalDigitMap.get(p2) ?? ""));
     }
 
     static evaluateLine(line: string): number {
-        const fistDigit = [...Day1Part2.replaceFirstLiteralByDigit(line)].find(v => Number(v));
-        const lastDigit = [...Day1Part2.replaceLastLiteralByDigit(line)].reverse().find(v => Number(v));
-        if(fistDigit && lastDigit) {
-            return (+fistDigit) * 10 + (+lastDigit);
-        }
-        return 0;
+        const fistDigit = [...this.replaceFirstLiteralByDigit(line)].find(v => Number(v)) ?? "0";
+        const lastDigit = [...this.replaceLastLiteralByDigit(line)].reverse().find(v => Number(v)) ?? "0";
+        return (+fistDigit) * 10 + (+lastDigit);
     }
 
     static solve(input: string): number {
         const lines = input.split(/[\r\n]+/);
         return lines.reduce((acc, line) => acc + Day1Part2.evaluateLine(line), 0);
     }
+    
 }

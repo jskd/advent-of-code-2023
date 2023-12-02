35ab61d70cb03934d9b11f5aad1a7f826b682d45
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-02 13:38:49 +0100

    refacto + optim regex

--- src/day-01/day-1-part-2.ts
+++ src/day-01/day-1-part-2.ts
@@ -1,35 +1,24 @@
+type Direction = "leftToRight" | "rightToLeft";
+
 export abstract class Day1Part2 {
     static readonly literalDigitMap: Map<string, number> = new Map([
-        ["one", 1], 
-        ["two", 2],
-        ["three", 3],
-        ["four", 4],
-        ["five", 5],
-        ["six", 6],
-        ["seven",7],
-        ["eight", 8],
-        ["nine", 9],
-        ["1", 1], 
-        ["2", 2],
-        ["3", 3],
-        ["4", 4],
-        ["5", 5],
-        ["6", 6],
-        ["7",7],
-        ["8", 8],
-        ["9", 9]
+        ["one", 1], ["1", 1], ["two", 2], ["2", 2], ["three", 3], ["3", 3], 
+        ["four", 4], ["4", 4], ["five", 5],["5", 5], ["six", 6], ["6", 6],
+        ["seven", 7], ["7", 7], ["eight", 8], ["8", 8], ["nine", 9], ["9", 9]
     ]);
     static readonly literalTokens = [...this.literalDigitMap.keys()].join('|');
 
-    static getFirstDigit(input: string, direction: "leftToRight" | "rightToLeft"): number {
-        const lazy = new RegExp(`(?:.*?)(${this.literalTokens})`);
-        const greedy = new RegExp(`(?:.*)(${this.literalTokens})`);
+    static getFirstDigit(line: string, direction: Direction): number {
+        const lazy = new RegExp(`^(?:.*?)(${this.literalTokens})`);
+        const greedy = new RegExp(`^(?:.*)(${this.literalTokens})`);
         const regex = direction === "leftToRight" ? lazy : greedy;
-        return this.literalDigitMap.get( input.match(regex)?.[1] ?? "" ) ?? 0;
+        const [_, literal] = line.match(regex) ?? [""];
+        return this.literalDigitMap.get( literal ) ?? 0;
     }
 
     static solve(input: string): number {
-        const lines = input.split(/[\r\n]+/);
-        return lines.reduce((acc, line) => acc + this.getFirstDigit(line, "leftToRight") * 10 + this.getFirstDigit(line, "rightToLeft"), 0);
+        return input.split(/[\r\n]+/).reduce((acc, line) => 
+            acc + this.getFirstDigit(line, "leftToRight")*10 + this.getFirstDigit(line, "rightToLeft")
+        , 0);
     }
 }

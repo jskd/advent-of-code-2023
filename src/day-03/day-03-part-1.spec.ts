83cad0ff38bd891ff146a4b6b6c3448fa17e1421
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-02 17:45:46 +0100

    preparation for day 3

---
+++ src/day-03/day-03-part-1.spec.ts
@@ -0,0 +1,10 @@
+import { Day03Part1 } from "./day-03-part-1"
+import fs from 'fs'
+
+describe("Day 3 part 1", function() {
+    
+    it("Solve day 3 part 1", function() {
+        //const content = fs.readFileSync(`${__dirname}/day-03-input.txt`);
+        //expect(Day03Part1.solve(content.toString())).toBe(       );
+    });
+});
---
+++ src/day-03/day-03-part-2.spec.ts
@@ -0,0 +1,10 @@
+import { Day03Part2 } from "./day-03-part-2"
+import fs from 'fs'
+
+describe("Day 3 part 2", function() {
+    
+    it("Solve day 3 part 2", function() {
+        //const content = fs.readFileSync(`${__dirname}/day-03-input.txt`);
+        //expect(Day03Part2.solve(content.toString())).toBe(       );
+    });
+});

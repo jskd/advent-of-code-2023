4e139e6b64d64eefcae972e598a4d88887a0ca1c
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-04 11:17:42 +0100

    find gear

--- src/day-03/day-03-part-2.spec.ts
+++ src/day-03/day-03-part-2.spec.ts
@@ -2,6 +2,26 @@ import { Day03Part2 } from "./day-03-part-2"
 import fs from 'fs'
 
 describe("Day 3 part 2", function() {
+
+    const example = "467..114..\r\n" +
+            "...*......\r\n" +
+            "..35..633.\r\n" +
+            "......#...\r\n" +
+            "617*......\r\n" +
+            ".....+.58.\r\n" +
+            "..592.....\r\n" +
+            "......755.\r\n" +
+            "...$.*....\r\n" +
+            ".664.598.." ;
+    const gears = [
+        { x: 1, y: 3, numbers: [] },
+        { x: 4, y: 3, numbers: [] },
+        { x: 8, y: 5, numbers: [] }
+    ];
+
+    it("Find symbols position", function() {
+        expect(Day03Part2.findGearPosition(example)).toStrictEqual(gears);
+    });
     
     it("Solve day 3 part 2", function() {
         //const content = fs.readFileSync(`${__dirname}/day-03-input.txt`);
--- src/day-03/day-03-part-2.ts
+++ src/day-03/day-03-part-2.ts
@@ -1,2 +1,25 @@
+interface GearPosition {
+    x: number;
+    y: number;
+    numbers: string[];
+}
+
+
 export abstract class Day03Part2 {
+
+    static findGearPosition(input: string): GearPosition[] {
+        return input.split(/[\r\n]+/).filter(Boolean)
+            .flatMap((line, lineNumber) => 
+                [ ...line.matchAll(/\*/g) ].map(({index}): GearPosition => ({
+                    x: lineNumber,
+                    y: index!,
+                    numbers: []
+                })) 
+            );
+    }
+
+
+
+
+    
 }

28e37549f6b413671730c11ccd4c6b8055f880d0
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-04 14:58:49 +0100

    solve day 2 part 2

--- src/day-03/day-03-part-2.spec.ts
+++ src/day-03/day-03-part-2.spec.ts
@@ -1,8 +1,7 @@
-import { Day03Part2 } from "./day-03-part-2"
+import { Day03Part2, GearPosition } from "./day-03-part-2"
 import fs from 'fs'
 
 describe("Day 3 part 2", function() {
-
     const example = "467..114..\r\n" +
             "...*......\r\n" +
             "..35..633.\r\n" +
@@ -14,17 +13,58 @@ describe("Day 3 part 2", function() {
             "...$.*....\r\n" +
             ".664.598.." ;
     const gears = [
-        { x: 1, y: 3, numbers: [] },
-        { x: 4, y: 3, numbers: [] },
-        { x: 8, y: 5, numbers: [] }
+        new GearPosition(1, 3),
+        new GearPosition(4, 3),
+        new GearPosition(8, 5),
     ];
+    const numbers = [
+        {"value": "467", "x": 0, "y": 0}, 
+        {"value": "114", "x": 0, "y": 5}, 
+        {"value": "35", "x": 2, "y": 2}, 
+        {"value": "633", "x": 2, "y": 6}, 
+        {"value": "617", "x": 4, "y": 0}, 
+        {"value": "58", "x": 5, "y": 7}, 
+        {"value": "592", "x": 6, "y": 2}, 
+        {"value": "755", "x": 7, "y": 6}, 
+        {"value": "664", "x": 9, "y": 1}, 
+        {"value": "598", "x": 9, "y": 5}
+    ];
+    const associatedGear = [
+        new GearPosition(1, 3, [numbers[0], numbers[2]]),
+        new GearPosition(4, 3, [numbers[4]]),
+        new GearPosition(8, 5, [numbers[7], numbers[9]]),
+    ]
 
-    it("Find symbols position", function() {
+    it("Find gear position", function() {
         expect(Day03Part2.findGearPosition(example)).toStrictEqual(gears);
     });
-    
+
+    it("Associate number adjacent to gear", function() {
+        let tested_gears = [
+            new GearPosition(1, 3),
+            new GearPosition(4, 3),
+            new GearPosition(8, 5),
+        ];
+        numbers.forEach(pos => Day03Part2.associateGearAdjacent(pos, tested_gears));
+        expect(tested_gears).toStrictEqual(associatedGear);
+    });
+
+    it("calculate gear ratio", function() {
+        expect(associatedGear[0].getRatio()).toBe(16345);
+        expect(associatedGear[1].getRatio()).toBe(0);
+        expect(associatedGear[2].getRatio()).toBe(451490);
+    });
+
+    it("calculate sum of gear ratio", function() {
+        expect( Day03Part2.getSumGearRatio(associatedGear)).toBe(467835);
+    });
+
+    it("solve example", function() {
+        expect(Day03Part2.solve(example)).toBe(467835);
+    });
+
     it("Solve day 3 part 2", function() {
-        //const content = fs.readFileSync(`${__dirname}/day-03-input.txt`);
-        //expect(Day03Part2.solve(content.toString())).toBe(       );
+        const content = fs.readFileSync(`${__dirname}/day-03-input.txt`);
+        expect(Day03Part2.solve(content.toString())).toBe(86879020);
     });
 });
--- src/day-03/day-03-part-2.ts
+++ src/day-03/day-03-part-2.ts
@@ -1,25 +1,72 @@
-interface GearPosition {
-    x: number;
-    y: number;
-    numbers: string[];
+export class GearPosition {
+    x = 0;
+    y = 0;
+    numbers: NumberPosition[];
+    
+    constructor(x: number, y: number, numbers: NumberPosition[] = []) {
+        this.x = x;
+        this.y = y;
+        this.numbers = numbers;
+    }
+ 
+    getRatio(): number {
+        return this.numbers.length === 2
+            ? +this.numbers[0].value * +this.numbers[1].value
+            : 0;
+    } 
+
+    isAdjacent({x, y, value}: NumberPosition) {
+        return x-1 <= this.x && this.x <= x+1 
+            && y-1 <= this.y && this.y <= y+value.length;
+    }
+
+    pushNumber(numberPos: NumberPosition) {
+        this.numbers.push(numberPos);
+    }
 }
 
+export class NumberPosition {
+    x: number;
+    y: number;
+    value: string;
+
+    constructor(x: number, y: number, value: string) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+ 
+}
 
 export abstract class Day03Part2 {
 
     static findGearPosition(input: string): GearPosition[] {
         return input.split(/[\r\n]+/).filter(Boolean)
             .flatMap((line, lineNumber) => 
-                [ ...line.matchAll(/\*/g) ].map(({index}): GearPosition => ({
-                    x: lineNumber,
-                    y: index!,
-                    numbers: []
-                })) 
-            );
+                [ ...line.matchAll(/\*/g) ].map(({index}) => new GearPosition(lineNumber, index!)
+            ));
     }
 
+    static findNumberPosition(input: string): NumberPosition[] {
+        return input.split(/[\r\n]+/).filter(Boolean)
+            .flatMap((line, lineNumber) => 
+                [ ...line.matchAll(/\d+/g) ].map((match) => new NumberPosition(lineNumber, match.index!, match[0]!)
+            ));
+    }
 
+    static associateGearAdjacent(numberPosition: NumberPosition, gears: GearPosition[]) {
+        gears.filter(gear => gear.isAdjacent(numberPosition))
+            .forEach(gear => gear.pushNumber(numberPosition));
+    }
 
+    static getSumGearRatio(gears: GearPosition[]): number {
+        return gears.reduce((acc, gear) => acc + gear.getRatio(), 0)
+    }
 
-    
+    static solve(input: string) {
+        let gears = this.findGearPosition(input);
+        let numbers = this.findNumberPosition(input);
+        numbers.forEach(pos => this.associateGearAdjacent(pos, gears));
+        return this.getSumGearRatio(gears);
+    }
 }

594d6a4b652e77ed9735c869423b83662877f6c7
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-08 19:10:36 +0100

    clean up

--- src/day-07/day-07.spec.ts
+++ src/day-07/day-07.spec.ts
@@ -35,33 +35,29 @@ fdescribe("Day 7", function () {
   );
 
   it("Sort by strength without joker", function () {
-    const inputs = [
-      new Hand("444J3", 365, false),
-      new Hand("32T3K", 136, false),
-      new Hand("Q2TJK", 488, false),
-      new Hand("JKKJJ", 66, false),
-      new Hand("J58JJ", 650, false),
-    ];
-    const hands = new Hands(...inputs);
-    expect(hands.sortByStrength().map(({ cards }) => cards)).toStrictEqual([
-      "Q2TJK",
-      "32T3K",
-      "444J3",
-      "J58JJ",
-      "JKKJJ",
-    ]);
+    expect(
+      new Hands(
+        new Hand("444J3", 365, false),
+        new Hand("32T3K", 136, false),
+        new Hand("Q2TJK", 488, false),
+        new Hand("JKKJJ", 66, false),
+        new Hand("J58JJ", 650, false)
+      )
+        .sortByStrength()
+        .map(({ cards }) => cards)
+    ).toStrictEqual(["Q2TJK", "32T3K", "444J3", "J58JJ", "JKKJJ"]);
   });
 
   it("Get total bid without joker", function () {
-    const inputs = [
-      new Hand("444J3", 365, false),
-      new Hand("32T3K", 136, false),
-      new Hand("Q2TJK", 488, false),
-      new Hand("JKKJJ", 66, false),
-      new Hand("J58JJ", 650, false),
-    ];
-    const hands = new Hands(...inputs);
-    expect(hands.getTotalBid()).toBe(4785);
+    expect(
+      new Hands(
+        new Hand("444J3", 365, false),
+        new Hand("32T3K", 136, false),
+        new Hand("Q2TJK", 488, false),
+        new Hand("JKKJJ", 66, false),
+        new Hand("J58JJ", 650, false)
+      ).getTotalBid()
+    ).toBe(4785);
   });
 
   test.each([
@@ -101,33 +97,29 @@ fdescribe("Day 7", function () {
   );
 
   it("Sort by strength with joker", function () {
-    const inputs = [
-      new Hand("444J3", 365, true),
-      new Hand("32T3K", 136, true),
-      new Hand("Q2TJK", 488, true),
-      new Hand("JKKJJ", 66, true),
-      new Hand("J58JJ", 650, true),
-    ];
-    const hands = new Hands(...inputs);
-    expect(hands.sortByStrength().map(({ cards }) => cards)).toStrictEqual([
-      "32T3K",
-      "Q2TJK",
-      "J58JJ",
-      "444J3",
-      "JKKJJ",
-    ]);
+    expect(
+      new Hands(
+        new Hand("444J3", 365, true),
+        new Hand("32T3K", 136, true),
+        new Hand("Q2TJK", 488, true),
+        new Hand("JKKJJ", 66, true),
+        new Hand("J58JJ", 650, true)
+      )
+        .sortByStrength()
+        .map(({ cards }) => cards)
+    ).toStrictEqual(["32T3K", "Q2TJK", "J58JJ", "444J3", "JKKJJ"]);
   });
 
   it("Get total bid without joker", function () {
-    const inputs = [
-      new Hand("444J3", 365, true),
-      new Hand("32T3K", 136, true),
-      new Hand("Q2TJK", 488, true),
-      new Hand("JKKJJ", 66, true),
-      new Hand("J58JJ", 650, true),
-    ];
-    const hands = new Hands(...inputs);
-    expect(hands.getTotalBid()).toBe(4852);
+    expect(
+      new Hands(
+        new Hand("444J3", 365, true),
+        new Hand("32T3K", 136, true),
+        new Hand("Q2TJK", 488, true),
+        new Hand("JKKJJ", 66, true),
+        new Hand("J58JJ", 650, true)
+      ).getTotalBid()
+    ).toBe(4852);
   });
 
   // prettier-ignore

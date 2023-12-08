f8e5ca5e229c83e3403820bcfe6db9558ec24eaa
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-08 15:45:48 +0100

    solve day 7 part 2

--- src/day-07/day-07.spec.ts
+++ src/day-07/day-07.spec.ts
@@ -1,14 +1,50 @@
 import { Day7, Hand } from "./day-07";
 import fs from "fs";
+
 fdescribe("Day 7", function () {
-  it("Evaluate matching score", function () {
-    expect(Hand.getScoreMatching([1, 1, 1, 1, 1])).toBe(6);
-    expect(Hand.getScoreMatching([1, 1, 1, 1, 2])).toBe(5);
-    expect(Hand.getScoreMatching([1, 1, 1, 2, 2])).toBe(4);
-    expect(Hand.getScoreMatching([1, 1, 1, 2, 3])).toBe(3);
-    expect(Hand.getScoreMatching([1, 1, 2, 2, 3])).toBe(2);
-    expect(Hand.getScoreMatching([1, 1, 2, 3, 4])).toBe(1);
-    expect(Hand.getScoreMatching([1, 2, 3, 4, 5])).toBe(0);
+  it("Get strengh match", function () {
+    expect(Hand.getStrenghtMatch([1, 1, 1, 1, 1])).toBe(6);
+    expect(Hand.getStrenghtMatch([1, 1, 1, 1, 2])).toBe(5);
+    expect(Hand.getStrenghtMatch([1, 1, 1, 2, 2])).toBe(4);
+    expect(Hand.getStrenghtMatch([1, 1, 1, 2, 3])).toBe(3);
+    expect(Hand.getStrenghtMatch([1, 1, 2, 2, 3])).toBe(2);
+    expect(Hand.getStrenghtMatch([1, 1, 2, 3, 4])).toBe(1);
+    expect(Hand.getStrenghtMatch([1, 2, 3, 4, 5])).toBe(0);
+  });
+
+  it("Get strengh order", function () {
+    expect(Hand.getStrenghtOrder([1, 4, 1, 9, 9])).toBe(0x14199);
+    expect(Hand.getStrenghtOrder([4, 7, 4, 1, 3])).toBe(0x47413);
+  });
+
+  it("Get strengh hand", function () {
+    expect(Hand.getStrengthHand([1, 1, 1, 1, 1], false)).toBe(0x611111);
+    expect(Hand.getStrengthHand([1, 1, 1, 1, 2], false)).toBe(0x511112);
+    expect(Hand.getStrengthHand([1, 1, 1, 2, 2], false)).toBe(0x411122);
+    expect(Hand.getStrengthHand([1, 1, 1, 2, 3], false)).toBe(0x311123);
+    expect(Hand.getStrengthHand([1, 1, 2, 2, 3], false)).toBe(0x211223);
+    expect(Hand.getStrengthHand([1, 1, 2, 3, 4], false)).toBe(0x111234);
+    expect(Hand.getStrengthHand([1, 2, 3, 4, 5], false)).toBe(0x012345);
+  });
+
+  it("Map joker as wildcard", function () {
+    const J = Hand.J;
+    expect(Hand.mapJokerAsWildcard([J, 2, 2, 9, 9])).toStrictEqual([
+      2, 2, 2, 9, 9,
+    ]);
+    expect(Hand.mapJokerAsWildcard([J, 2, 9, 9, 9])).toStrictEqual([
+      9, 2, 9, 9, 9,
+    ]);
+  });
+
+  it("Map joker as weakest", function () {
+    const J = Hand.J;
+    expect(Hand.mapJokerAsWeakest([J, 2, 2, J, 9])).toStrictEqual([
+      1, 2, 2, 1, 9,
+    ]);
+    expect(Hand.mapJokerAsWeakest([J, 2, 9, 9, 9])).toStrictEqual([
+      1, 2, 9, 9, 9,
+    ]);
   });
 
   it("Solve Day 7 part 1 example ", function () {
@@ -18,11 +54,26 @@ fdescribe("Day 7", function () {
                     "KK677 28\r\n" + 
                     "KTJJT 220\r\n" + 
                     "QQQJA 483";
-    expect(Day7.solve(example)).toBe(6440);
+    expect(Day7.solve(example, false)).toBe(6440);
+  });
+
+  it("Solve Day 7 part 2 example ", function () {
+    // prettier-ignore
+    const example = "32T3K 765\r\n" + 
+                    "T55J5 684\r\n" + 
+                    "KK677 28\r\n" + 
+                    "KTJJT 220\r\n" + 
+                    "QQQJA 483";
+    expect(Day7.solve(example, true)).toBe(5905);
   });
 
   it("Solve Day 7 part 1 puzzle", function () {
     const content = fs.readFileSync(`${__dirname}/day-07-input.txt`);
-    expect(Day7.solve(content.toString())).toBe(253638586);
+    expect(Day7.solve(content.toString(), false)).toBe(253638586);
+  });
+
+  it("Solve Day 7 part 2 puzzle", function () {
+    const content = fs.readFileSync(`${__dirname}/day-07-input.txt`);
+    expect(Day7.solve(content.toString(), true)).toBe(253253225);
   });
 });
--- src/day-07/day-07.ts
+++ src/day-07/day-07.ts
@@ -1,27 +1,46 @@
 export class Hand {
-  readonly strength: number;
-
   // prettier-ignore
   static readonly cardValue: Record<string, number> = {
-    "2": 0x0, "3": 0x1, "4": 0x2, "5": 0x3, "6": 0x4, "7": 0x5, "8": 0x6, "9": 0x7, 
-    "T": 0x8, "J": 0x9, "Q": 0xa, "K": 0xb, "A": 0xc
+    "2": 0x2, "3": 0x3, "4": 0x4, "5": 0x5, "6": 0x6, "7": 0x7, "8": 0x8, "9": 0x9, 
+    "T": 0xa, "J": 0xb, "Q": 0xc, "K": 0xe, "A": 0xf
   };
+  static readonly J = Hand.cardValue["J"];
+  readonly strength: number;
 
   constructor(
     readonly cards: string,
-    readonly bid: number
+    readonly bid: number,
+    withJoker: boolean
   ) {
-    const strenghtCards = Hand.mapCardStrength(cards);
-    this.strength =
-      (Hand.getStrenghtMatch(strenghtCards) << 20) |
-      Hand.getStrenghtOrder(strenghtCards);
+    this.strength = Hand.getStrengthHand(
+      Hand.mapCardStrength(cards),
+      withJoker
+    );
   }
 
   static mapCardStrength(cards: string): number[] {
     return [...cards].map((cp) => Hand.cardValue[cp]);
   }
 
-  static getStrenghtMatch(cards: number[]) {
+  static mapJokerAsWildcard(cards: number[]) {
+    const matchs: Record<string, number> = {};
+    cards
+      .filter((v) => v != Hand.J)
+      .forEach((card) => (matchs[card] = (matchs[card] || 0) + 1));
+
+    const bestMatch = Object.entries(matchs)
+      .sort((a, b) => b[1] - a[1]) // sort by key in reverse order
+      .shift()?.[0];
+    return !bestMatch
+      ? cards
+      : cards.map((card) => (card == Hand.J ? +bestMatch : card));
+  }
+
+  static mapJokerAsWeakest(cards: number[]): number[] {
+    return cards.map((card) => (card == Hand.J ? 0x1 : card));
+  }
+
+  static getStrenghtMatch(cards: number[]): number {
     const matchs: Record<string, number> = {};
     cards.forEach((card) => (matchs[card] = (matchs[card] || 0) + 1));
     const [high, low] = Object.values(matchs).sort().reverse();
@@ -34,22 +53,24 @@ export class Hand {
     else return 0;
   }
 
-  static getStrenghtOrder(cards: number[]) {
+  static getStrenghtOrder(cards: number[]): number {
     return cards.reduce((acc, card) => (acc << 4) | card, 0);
+  }
+
+  static getStrengthHand(cards: number[], withJoker: boolean) {
+    const order = Hand.getStrenghtOrder(
+      withJoker ? Hand.mapJokerAsWeakest(cards) : cards
+    );
+    const match = Hand.getStrenghtMatch(
+      withJoker ? Hand.mapJokerAsWildcard(cards) : cards
+    );
+    return (match << 20) | order;
   }
 }
 
 class Hands extends Array<Hand> {
-  constructor(hands: Hand[]) {
-    super(...hands);
-  }
-
   sortByStrength(): Hands {
-    return this.sort(({ strength: a }, { strength: b }) => {
-      if (a > b) return 1;
-      if (a < b) return -1;
-      return 0;
-    });
+    return this.sort(({ strength: a }, { strength: b }) => a - b);
   }
 
   getTotalBid(): number {
@@ -58,14 +79,14 @@ class Hands extends Array<Hand> {
 }
 
 export class Day7 {
-  static solve(input: string): number {
+  static solve(input: string, withJoker: boolean): number {
     const hands = new Hands(
-      input
+      ...input
         .split(/[\r\n]+/)
         .filter(Boolean)
         .map((line) => {
           const [hand, bid] = line.split(/\s+/);
-          return new Hand(hand, +bid);
+          return new Hand(hand, +bid, withJoker);
         })
     );
     return hands.sortByStrength().getTotalBid();

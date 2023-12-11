7582b1207cd271a27e596ca22b82543c428c5271
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-11 20:24:08 +0100

    make more simple day 5 par 1

--- src/day-05/day-05-part-1.spec.ts
+++ src/day-05/day-05-part-1.spec.ts
@@ -1,74 +1,40 @@
-import { Day05Part1, Mapping, MappingList, MapEntry } from "./day-05-part-1";
+import { Day05Part1 } from "./day-05-part-1";
 import fs from "fs";
 
-describe("Day 5 part 1", function () {
-  const seedToSoilLines = ["50 98 2", "52 50 48"];
-  const soilToFertilizerLines = ["0 15 37", "37 52 2", "39 0 15"];
-  const fertilizerWaterLines = ["49 53 8", "0 11 42", "42 0 7", "57 7 4"];
-  const waterLight = ["88 18 7", "18 25 70"];
-  const lightTemperatureLines = ["45 77 23", "81 45 19", "68 64 13"];
-  const temperatureHumidityLines = ["0 69 1", "1 0 69"];
-  const humidityLocationLines = ["60 56 37", "56 93 4"];
-
-  test.each`
-    src                         | dst                         | expected
-    ${new MapEntry(10, 60, 10)} | ${new MapEntry(65, 10, 5)}  | ${new MapEntry(15, 10, 5)}
-    ${new MapEntry(10, 60, 5)}  | ${new MapEntry(60, 10, 10)} | ${new MapEntry(10, 10, 5)}
-    ${new MapEntry(10, 60, 5)}  | ${new MapEntry(0, 10, 5)}   | ${null}
-    ${new MapEntry(50, 20, 5)}  | ${new MapEntry(20, 50, 5)}  | ${new MapEntry(50, 50, 5)}
-  `("Get merged intersection $#", ({ src, dst, expected }) =>
-    expect(Mapping.getMergedIntersection(src, dst)).toStrictEqual(expected)
-  );
-
-  test.each`
-    src                         | merge                       | expected
-    ${new MapEntry(60, 20, 20)} | ${new MapEntry(50, 10, 40)} | ${[]}
-    ${new MapEntry(60, 20, 20)} | ${new MapEntry(20, 0, 20)}  | ${[new MapEntry(60, 20, 20)]}
-    ${new MapEntry(60, 20, 20)} | ${new MapEntry(60, 20, 10)} | ${[new MapEntry(70, 30, 10)]}
-    ${new MapEntry(60, 20, 20)} | ${new MapEntry(70, 20, 10)} | ${[new MapEntry(60, 20, 10)]}
-    ${new MapEntry(60, 20, 20)} | ${new MapEntry(65, 25, 10)} | ${[new MapEntry(60, 20, 5), new MapEntry(75, 35, 5)]}
-  `("Get remove intersection on source $#", ({ src, merge, expected }) =>
-    expect(Mapping.removeIntesection(src, merge, "src")).toStrictEqual(expected)
-  );
-
-  test.each`
-    dst                         | merge                       | expected
-    ${new MapEntry(20, 60, 20)} | ${new MapEntry(10, 50, 40)} | ${[]}
-    ${new MapEntry(20, 60, 20)} | ${new MapEntry(0, 20, 20)}  | ${[new MapEntry(20, 60, 20)]}
-    ${new MapEntry(20, 60, 20)} | ${new MapEntry(20, 60, 10)} | ${[new MapEntry(30, 70, 10)]}
-    ${new MapEntry(20, 60, 20)} | ${new MapEntry(20, 70, 10)} | ${[new MapEntry(20, 60, 10)]}
-    ${new MapEntry(20, 60, 20)} | ${new MapEntry(25, 65, 10)} | ${[new MapEntry(20, 60, 5), new MapEntry(35, 75, 5)]}
-  `("Get remove intersection on destination $#", ({ dst, merge, expected }) =>
-    expect(Mapping.removeIntesection(dst, merge, "dst")).toStrictEqual(expected)
-  );
-
-  it("Get corresponding destination", function () {
-    const mapping = new Mapping(seedToSoilLines);
-    expect(mapping.getDestination(79)).toEqual(81);
-    expect(mapping.getDestination(14)).toEqual(14);
-    expect(mapping.getDestination(55)).toEqual(57);
-    expect(mapping.getDestination(13)).toEqual(13);
+describe("Day 5", function () {
+  const example =
+    "seeds: 79 14 55 13\r\n" +
+    "seed-to-soil map:\r\n" +
+    "50 98 2\r\n" +
+    "52 50 48\r\n" +
+    "soil-to-fertilizer map:\r\n" +
+    "0 15 37\r\n" +
+    "37 52 2\r\n" +
+    "39 0 15\r\n" +
+    "fertilizer-to-water map:\r\n" +
+    "49 53 8\r\n" +
+    "0 11 42\r\n" +
+    "42 0 7\r\n" +
+    "57 7 4\r\n" +
+    "water-to-light map:\r\n" +
+    "88 18 7\r\n" +
+    "18 25 70\r\n" +
+    "light-to-temperature map:\r\n" +
+    "45 77 23\r\n" +
+    "81 45 19\r\n" +
+    "68 64 13\r\n" +
+    "temperature-to-humidity map:\r\n" +
+    "0 69 1\r\n" +
+    "1 0 69\r\n" +
+    "humidity-to-location map:\r\n" +
+    "60 56 37\r\n" +
+    "56 93 4";
+  it("Solve day 5 part 1 exemple", function () {
+    expect(Day05Part1.solve(example)).toBe(35);
   });
 
-  it("Get seeds location", function () {
-    const mappingList = new MappingList();
-    mappingList.push(
-      new Mapping(seedToSoilLines),
-      new Mapping(soilToFertilizerLines),
-      new Mapping(fertilizerWaterLines),
-      new Mapping(waterLight),
-      new Mapping(lightTemperatureLines),
-      new Mapping(temperatureHumidityLines),
-      new Mapping(humidityLocationLines)
-    );
-    expect(mappingList.getDestination(79)).toBe(82);
-    expect(mappingList.getDestination(14)).toBe(43);
-    expect(mappingList.getDestination(55)).toBe(86);
-    expect(mappingList.getDestination(13)).toBe(35);
-  });
-
-  it("Solve day 5 part 2", function () {
-    const content = fs.readFileSync(`${__dirname}/day-05-input.txt`);
-    expect(Day05Part1.solve(content.toString())).toBe(157211394);
+  const puzzle = fs.readFileSync(`${__dirname}/day-05-input.txt`).toString();
+  it("Solve day 5 part 1 puzzle", function () {
+    expect(Day05Part1.solve(puzzle)).toBe(157211394);
   });
 });
--- src/day-05/day-05-part-1.ts
+++ src/day-05/day-05-part-1.ts
@@ -1,119 +1,49 @@
-class Range {
-  constructor(
-    public start: number,
-    public end: number
-  ) {}
-}
+export class Range {
+  readonly start: number;
+  readonly end: number;
+  readonly offset: number;
 
-export class MapEntry {
-  public src: Range;
-  public dst: Range;
-  public offset: number;
-
-  constructor(srcStart: number, dstStart: number, len: number) {
-    this.src = new Range(srcStart, srcStart + len);
-    this.dst = new Range(dstStart, dstStart + len);
-    this.offset = dstStart - srcStart;
+  constructor(source: number, destination: number, len: number) {
+    this.start = source;
+    this.end = source + len;
+    this.offset = destination - source;
   }
 }
 
 export class Mapping {
-  maps: MapEntry[];
+  ranges: Range[] = [];
+  next?: Mapping;
 
-  constructor(lines: string[]) {
-    this.maps = lines.map((line) => {
-      const [dst, src, len] = line.split(/\s+/).map((v) => +v);
-      return new MapEntry(src, dst, len);
-    });
-  }
-
-  getDestination(source: number) {
-    const match = this.maps.find(
-      ({ src }) => src.start <= source && source < src.end
-    );
-    return match ? match.dst.start + (source - match.src.start) : source;
-  }
-
-  static getMergedIntersection(src: MapEntry, dst: MapEntry) {
-    if (src.dst.end <= dst.src.start || src.dst.start >= dst.src.end) {
-      return null;
-    }
-    const start = Math.max(src.dst.start, dst.src.start);
-    const len = Math.min(src.dst.end, dst.src.end) - start;
-    return new MapEntry(start - src.offset, start + dst.offset, len);
-  }
-
-  static removeIntesection(
-    sourceEntry: MapEntry,
-    mergeEntry: MapEntry,
-    mode: "src" | "dst"
-  ) {
-    const [source, merge] =
-      mode == "src"
-        ? [sourceEntry.src, mergeEntry.src]
-        : [sourceEntry.dst, mergeEntry.dst];
-
-    if (source.end <= merge.start || source.start >= merge.end) {
-      return [sourceEntry];
-    }
-
-    const result: MapEntry[] = [];
-    if (source.end > merge.start && merge.start > source.start) {
-      result.push(
-        new MapEntry(
-          sourceEntry.src.start,
-          sourceEntry.dst.start,
-          merge.start - source.start
-        )
-      );
-    }
-    if (source.end > merge.end && source.end > merge.end) {
-      const overlap = source.end - merge.end;
-      result.push(
-        new MapEntry(
-          sourceEntry.src.end - overlap,
-          sourceEntry.dst.end - overlap,
-          overlap
-        )
-      );
-    }
-    return result;
-  }
-}
-
-export class MappingList extends Array<Mapping> {
   getDestination(source: number): number {
-    return this.reduce((acc, mapping) => mapping.getDestination(acc), source);
+    source +=
+      this.ranges.find(({ start, end }) => start <= source && source < end)
+        ?.offset ?? 0;
+    return this.next ? this.next.getDestination(source) : source;
   }
 }
 
 export abstract class Day05Part1 {
   static solve(input: string): number {
-    const lines = input.split(/[\r\n]+/).filter(Boolean);
-    const [_, seedsStr] = lines.shift()!.split(":");
-    const seeds = seedsStr
+    const lines = input.split(/[\r\n]+/);
+    const seeds = lines[0]
       .split(" ")
-      .filter(Boolean)
-      .map((number) => +number);
+      .map((number) => +number)
+      .filter(Boolean);
 
-    const mappingList = new MappingList();
-    const mappingEntry = [];
-
+    const start = new Mapping();
+    let currentMapping = start;
     for (const line of lines) {
       if (/^\d/.test(line)) {
-        mappingEntry.push(line);
-      } else if (mappingEntry.length) {
-        mappingList.push(new Mapping(mappingEntry));
-        mappingEntry.length = 0;
+        const [dst, src, len] = line.split(" ").map((value) => +value);
+        currentMapping.ranges.push(new Range(src, dst, len));
+      } else if (currentMapping.ranges.length) {
+        currentMapping.next = new Mapping();
+        currentMapping = currentMapping.next;
       }
-    }
-    if (mappingEntry.length) {
-      mappingList.push(new Mapping(mappingEntry));
-      mappingEntry.length = 0;
     }
 
     return seeds.reduce(
-      (acc, seed) => Math.min(mappingList.getDestination(seed), acc),
+      (acc, seed) => Math.min(start.getDestination(seed), acc),
       Infinity
     );
   }

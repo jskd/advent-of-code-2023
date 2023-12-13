068fd9ffaff6909d6ce5e5341476b69aaeba2035
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-13 20:53:17 +0100

    day 12 part 1 evaluate group

--- src/day-12/day-12.spec.ts
+++ src/day-12/day-12.spec.ts
@@ -1,4 +1,5 @@
-import { evaluateArrangement } from "./day-12";
+import { Day12, evaluate, evaluateArrangement } from "./day-12";
+import fs from "fs";
 
 describe("Day 12", function () {
   it("Evaluate arrangement", function () {
@@ -17,5 +18,24 @@ describe("Day 12", function () {
     expect(evaluateArrangement("??##?#??", 4)).toBe(1);
     expect(evaluateArrangement("??##?#??", 5)).toBe(2);
     expect(evaluateArrangement("??##?#??", 6)).toBe(3);
+    expect(evaluateArrangement("??####??", 3)).toBe(0);
+    expect(evaluateArrangement("########", 8)).toBe(1);
   });
+
+  it("Evaluate arrangement", function () {
+    expect(evaluate("?#?#?#", [1, 3])).toBe(1);
+    expect(evaluate("?#?#?#", [3, 1])).toBe(1);
+    expect(evaluate("?#?#?#?#", [1, 3, 1])).toBe(1);
+    expect(evaluate("?#?#?#?#?", [1, 3, 1])).toBe(1);
+    expect(evaluate("?#?#?#?#?#?#?#?", [1, 3, 1, 6])).toBe(1);
+    expect(evaluate("?###????", [3, 2, 1])).toBe(0);
+    expect(evaluate("?###?????", [3, 2, 1])).toBe(1);
+    expect(evaluate("?###??????", [3, 2, 1])).toBe(3);
+    expect(evaluate("?###????????", [3, 2, 1])).toBe(10);
+  });
+
+  /*it("Solve day 2 part 2", function () {
+    const content = fs.readFileSync(`${__dirname}/day-12-input.txt`);
+    expect(Day12.solve(content.toString())).toBe(65122);
+  });*/
 });
--- src/day-12/day-12.ts
+++ src/day-12/day-12.ts
@@ -10,13 +10,55 @@ export function evaluateArrangement(line: string, group: number) {
     return line.length - group + 1;
   }
   const end = line.lastIndexOf("#");
+  if (end - start > group) {
+    return 0;
+  }
+
   const lowStart = Math.max(end - group + 1, 0);
   const highStart = Math.min(start, line.length - group);
   return highStart - lowStart + 1;
 }
 
-export class Day12 {
-  static solve(_input: string): number {
+export function evaluateGroupArrangement(
+  line: string,
+  group: number[]
+): number {
+  if (group.length === 1) {
+    return evaluateArrangement(line, group[0]);
+  }
+
+  if (line.charAt(group[0]) == "#") {
     return 0;
+  }
+
+  return (
+    evaluateArrangement(line.substring(0, group[0]), group[0]) *
+    evaluateGroupArrangement(line.substring(group[0] + 1), group.slice(1))
+  );
+}
+
+export function evaluate(line: string, group: number[]) {
+  let posibilities = 0;
+  for (let i = 0; i < line.length; i++) {
+    posibilities += evaluateGroupArrangement(line.substring(i), group);
+  }
+
+  return posibilities;
+}
+
+export class Day12 {
+  static solve(input: string): number {
+    return input
+      .split(/[\r\n]+/)
+      .filter(Boolean)
+      .flatMap((line) =>
+        line.split(" ").map(([sequence, group]) =>
+          evaluate(
+            sequence,
+            group.split(",").map((v) => +v)
+          )
+        )
+      )
+      .reduce((acc, line) => acc + line, 0);
   }
 }

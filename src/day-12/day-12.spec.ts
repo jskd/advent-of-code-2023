b9a6c88c82d3ea88ac21b8a1648e184b34c2d81e
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-14 01:48:42 +0100

    day 12 part 1 solved

--- src/day-12/day-12.spec.ts
+++ src/day-12/day-12.spec.ts
@@ -1,33 +1,33 @@
-import { Day12, evaluate, evaluateArrangement } from "./day-12";
+import { Day12, evaluate } from "./day-12";
 import fs from "fs";
 
 describe("Day 12", function () {
   it("Evaluate arrangement", function () {
-    expect(evaluateArrangement("????????", 5)).toBe(4);
-    expect(evaluateArrangement("????????", 3)).toBe(6);
-    expect(evaluateArrangement("????????", 9)).toBe(0);
-    expect(evaluateArrangement("???????#", 5)).toBe(1);
-    expect(evaluateArrangement("????#??#", 5)).toBe(1);
-    expect(evaluateArrangement("????##?#", 5)).toBe(1);
-    expect(evaluateArrangement("#???????", 5)).toBe(1);
-    expect(evaluateArrangement("#??#????", 5)).toBe(1);
-    expect(evaluateArrangement("##?#????", 5)).toBe(1);
-    expect(evaluateArrangement("???#????", 5)).toBe(4);
-    expect(evaluateArrangement("???#??#?", 5)).toBe(2);
-    expect(evaluateArrangement("??##?#??", 5)).toBe(2);
-    expect(evaluateArrangement("??##?#??", 4)).toBe(1);
-    expect(evaluateArrangement("??##?#??", 5)).toBe(2);
-    expect(evaluateArrangement("??##?#??", 6)).toBe(3);
-    expect(evaluateArrangement("??####??", 3)).toBe(0);
-    expect(evaluateArrangement("########", 8)).toBe(1);
+    expect(evaluate("????????", [5])).toBe(4);
+    expect(evaluate("????????", [3])).toBe(6);
+    expect(evaluate("????????", [9])).toBe(0);
+    expect(evaluate("???????#", [5])).toBe(1);
+    expect(evaluate("????#??#", [5])).toBe(1);
+    expect(evaluate("????##?#", [5])).toBe(1);
+    expect(evaluate("#???????", [5])).toBe(1);
+    expect(evaluate("#??#????", [5])).toBe(1);
+    expect(evaluate("##?#????", [5])).toBe(1);
+    expect(evaluate("???#????", [5])).toBe(4);
+    expect(evaluate("???#??#?", [5])).toBe(2);
+    expect(evaluate("??##?#??", [5])).toBe(2);
+    expect(evaluate("??##?#??", [4])).toBe(1);
+    expect(evaluate("??##?#??", [5])).toBe(2);
+    expect(evaluate("??##?#??", [6])).toBe(3);
+    expect(evaluate("??####??", [3])).toBe(0);
+    expect(evaluate("########", [8])).toBe(1);
 
-    expect(evaluateArrangement("###.####", 8)).toBe(0);
-    expect(evaluateArrangement("???.????", 3)).toBe(3);
-    expect(evaluateArrangement("??#.????", 3)).toBe(1);
-    expect(evaluateArrangement("??#.??#?", 3)).toBe(0);
-    expect(evaluateArrangement("?#?.??.?", 2)).toBe(2);
-    expect(evaluateArrangement("?#?.??.?", 3)).toBe(1);
-    expect(evaluateArrangement("??...?.?", 1)).toBe(4);
+    expect(evaluate("###.####", [8])).toBe(0);
+    expect(evaluate("???.????", [3])).toBe(3);
+    expect(evaluate("??#.????", [3])).toBe(1);
+    expect(evaluate("??#.??#?", [3])).toBe(0);
+    expect(evaluate("?#?.??.?", [2])).toBe(2);
+    expect(evaluate("?#?.??.?", [3])).toBe(1);
+    expect(evaluate("??...?.?", [1])).toBe(4);
   });
 
   it("Evaluate arrangement", function () {
@@ -42,8 +42,8 @@ describe("Day 12", function () {
     expect(evaluate("?###????????", [3, 2, 1])).toBe(10);
   });
 
-  /*it("Solve day 2 part 2", function () {
+  it("Solve day 2 part 2", function () {
     const content = fs.readFileSync(`${__dirname}/day-12-input.txt`);
-    expect(Day12.solve(content.toString())).toBe(65122);
-  });*/
+    expect(Day12.solve(content.toString())).toBe(7110);
+  });
 });
--- src/day-12/day-12.ts
+++ src/day-12/day-12.ts
@@ -1,60 +1,26 @@
-export type Tile = "#" | "?";
-
-export function evaluateArrangement(line: string, group: number): number {
-  if (line.length < group) {
-    return 0;
+export function evaluate(line: string, group: number[]): number {
+  if (!line.length) {
+    return !group.length ? 1 : 0;
+  }
+  if (!group.length) {
+    return line.includes("#") ? 0 : 1;
   }
 
-  if (line.includes(".")) {
-    const lines = line.split(".").filter(Boolean);
-    const withShap = lines.filter((line) => line.includes("#"));
-    if (withShap.length > 1) {
-      return 0;
-    } else if (withShap.length === 1) {
-      return evaluateArrangement(withShap[0], group);
-    } else {
-      return lines.reduce((acc, v) => acc + evaluateArrangement(v, group), 0);
-    }
-  }
-
-  const start = line.indexOf("#");
-  if (start === -1) {
-    return line.length - group + 1;
-  }
-  const end = line.lastIndexOf("#");
-  if (end - start > group) {
-    return 0;
-  }
-
-  const lowStart = Math.max(end - group + 1, 0);
-  const highStart = Math.min(start, line.length - group);
-  return highStart - lowStart + 1;
-}
-
-export function evaluateGroupArrangement(
-  line: string,
-  group: number[]
-): number {
-  if (group.length === 1) {
-    return evaluateArrangement(line, group[0]);
-  }
-
-  if (line.charAt(group[0]) == "#") {
-    return 0;
-  }
-
-  return (
-    evaluateArrangement(line.substring(0, group[0]), group[0]) *
-    evaluateGroupArrangement(line.substring(group[0] + 1), group.slice(1))
-  );
-}
-
-export function evaluate(line: string, group: number[]) {
   let posibilities = 0;
   for (let i = 0; i < line.length; i++) {
-    posibilities += evaluateGroupArrangement(line.substring(i), group);
-  }
+    const before = line.substring(0, i);
+    const current = line.substring(i, i + group[0]);
+    const separator = line.charAt(i + group[0]);
+    const after = line.substring(i + group[0] + 1);
 
+    if (current.length < group[0] || before.includes("#")) {
+      return posibilities;
+    }
+    if (current.includes(".") || separator === "#") {
+      continue;
+    }
+    posibilities += evaluate(after, group.slice(1));
+  }
   return posibilities;
 }
 
@@ -63,14 +29,16 @@ export class Day12 {
     return input
       .split(/[\r\n]+/)
       .filter(Boolean)
-      .flatMap((line) =>
-        line.split(" ").map(([sequence, group]) =>
-          evaluate(
-            sequence,
-            group.split(",").map((v) => +v)
-          )
+      .map((line) => line.split(" "))
+      .map(([sequence, num]) =>
+        evaluate(
+          sequence,
+          num
+            .split(",")
+            .filter(Boolean)
+            .map((v) => +v)
         )
       )
-      .reduce((acc, line) => acc + line, 0);
+      .reduce((acc, val) => acc + val, 0);
   }
 }

9732adfb3d5ace4263fa1903eadc996e9c80b3fc
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-15 01:38:46 +0100

    clean up day 12

--- src/day-12/day-12.spec.ts
+++ src/day-12/day-12.spec.ts
@@ -1,4 +1,4 @@
-import { Day12, Day12Part2, evaluate } from "./day-12";
+import { Day12, evaluate } from "./day-12";
 import fs from "fs";
 
 describe("Day 12", function () {
@@ -20,7 +20,6 @@ describe("Day 12", function () {
     expect(evaluate("??##?#??", [6])).toBe(3);
     expect(evaluate("??####??", [3])).toBe(0);
     expect(evaluate("########", [8])).toBe(1);
-
     expect(evaluate("###.####", [8])).toBe(0);
     expect(evaluate("???.????", [3])).toBe(3);
     expect(evaluate("??#.????", [3])).toBe(1);
@@ -28,9 +27,6 @@ describe("Day 12", function () {
     expect(evaluate("?#?.??.?", [2])).toBe(2);
     expect(evaluate("?#?.??.?", [3])).toBe(1);
     expect(evaluate("??...?.?", [1])).toBe(4);
-  });
-
-  it("Evaluate arrangement", function () {
     expect(evaluate("?#?#?#", [1, 3])).toBe(1);
     expect(evaluate("?#?#?#", [3, 1])).toBe(1);
     expect(evaluate("?#?#?#?#", [1, 3, 1])).toBe(1);
@@ -42,13 +38,12 @@ describe("Day 12", function () {
     expect(evaluate("?###????????", [3, 2, 1])).toBe(10);
   });
 
-  it("Solve day 2 part 1", function () {
-    const content = fs.readFileSync(`${__dirname}/day-12-input.txt`);
-    expect(Day12.solve(content.toString())).toBe(7110);
+  const content = fs.readFileSync(`${__dirname}/day-12-input.txt`).toString();
+  it("Solve day 12 part 1", function () {
+    expect(Day12.solve(content)).toBe(7110);
   });
 
-  it("Solve day 2 part 2", function () {
-    const content = fs.readFileSync(`${__dirname}/day-12-input.txt`);
-    expect(Day12Part2.solve(content.toString())).toBe(1566786613613);
+  xit("Solve day 12 part 2", function () {
+    expect(Day12.solve(content, true)).toBe(1566786613613);
   });
 });
--- src/day-12/day-12.ts
+++ src/day-12/day-12.ts
@@ -1,71 +1,53 @@
 import memoizee from "memoizee";
 
-const memGetPosibilities = memoizee(getPosibilities, {
-  length: false,
-  primitive: true,
-});
+const getPosibilities = memoizee(
+  (line: string, groups: number[], counter = 0): number => {
+    if (!line) {
+      return groups.length || counter ? 0 : 1;
+    }
 
-function getPosibilities(line: string, group: number[], counter = 0): number {
-  if (!line.length) {
-    return group.length || counter ? 0 : 1;
-  }
-
-  let posibilities = 0;
-  const branchs = line[0] === "?" ? [".", "#"] : [line[0]];
-  for (const branch of branchs) {
-    if (branch === "#") {
-      posibilities += memGetPosibilities(line.slice(1), group, counter + 1);
-    } else {
-      if (!counter) {
-        posibilities += memGetPosibilities(line.slice(1), group);
-      }
-      if (group && group[0] == counter) {
-        posibilities += memGetPosibilities(line.slice(1), group.slice(1));
+    let posibilities = 0;
+    const branchs = line[0] === "?" ? [".", "#"] : [line[0]];
+    for (const branch of branchs) {
+      if (branch === "#") {
+        posibilities += getPosibilities(line.slice(1), groups, counter + 1);
+      } else {
+        if (!counter) {
+          posibilities += getPosibilities(line.slice(1), groups);
+        }
+        if (groups[0] == counter) {
+          posibilities += getPosibilities(line.slice(1), groups.slice(1));
+        }
       }
     }
+    return posibilities;
+  },
+  {
+    length: false,
+    primitive: true,
   }
-  return posibilities;
-}
+);
 
-export function evaluate(line: string, group: number[]): number {
-  return memGetPosibilities(line + ".", group, 0);
+export function evaluate(line: string, groups: number[]): number {
+  return getPosibilities(line + ".", groups);
 }
 
 export class Day12 {
-  static solve(input: string): number {
+  static solve(input: string, unfold = false): number {
     return input
       .split(/[\r\n]+/)
       .filter(Boolean)
       .map((line) => line.split(" "))
-      .map(([sequence, num]) =>
-        evaluate(
-          sequence,
-          num
-            .split(",")
-            .filter(Boolean)
-            .map((v) => +v)
-        )
-      )
-      .reduce((acc, val) => acc + val, 0);
-  }
-}
-
-export class Day12Part2 {
-  static solve(input: string): number {
-    return input
-      .split(/[\r\n]+/)
-      .filter(Boolean)
-      .map((line) => line.split(" "))
-      .map(([sequence, num]) => {
-        sequence = sequence + ("?" + sequence).repeat(4);
-        const groups = Array(5)
-          .fill(
-            num
-              .split(",")
-              .filter(Boolean)
-              .map((v) => +v)
-          )
-          .flat();
+      .map((part) => {
+        let sequence = part[0];
+        let groups = part[1]
+          .split(",")
+          .filter(Boolean)
+          .map((v) => +v);
+        if (unfold) {
+          sequence = sequence + ("?" + sequence).repeat(4);
+          groups = Array(5).fill(groups).flat();
+        }
         return evaluate(sequence, groups);
       })
       .reduce((acc, val) => acc + val, 0);

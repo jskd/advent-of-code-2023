f09ee2ab3263911682c14ea929ac478fbd302fb9
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-18 23:41:33 +0100

    day 15 part 2

--- src/day-15/day-15.spec.ts
+++ src/day-15/day-15.spec.ts
@@ -1,14 +1,20 @@
-import { Day15 } from "./day-15";
+import { Day15Part1, Day15Part2 } from "./day-15";
 import fs from "fs";
 
 describe("Day 15", () => {
   const example = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
   it("Solve day 15 part 1 exemple", () => {
-    expect(Day15.solve(example)).toBe(1320);
+    expect(Day15Part1.solve(example)).toBe(1320);
+  });
+  it("Solve day 15 part 2 exemple", () => {
+    expect(Day15Part2.solve(example)).toBe(145);
   });
 
   const puzzle = fs.readFileSync(`${__dirname}/day-15-input.txt`).toString();
   it("Solve day 15 part 1 puzzle", () => {
-    expect(Day15.solve(puzzle)).toBe(517315);
+    expect(Day15Part1.solve(puzzle)).toBe(517315);
+  });
+  it("Solve day 15 part 2 puzzle", () => {
+    expect(Day15Part2.solve(puzzle)).toBe(247763);
   });
 });
--- src/day-15/day-15.ts
+++ src/day-15/day-15.ts
@@ -1,15 +1,57 @@
+class Lens {
+  constructor(
+    readonly label: string,
+    public focal: number
+  ) {}
+}
+
 function hash(sequence: string): number {
   return sequence
     .split("")
     .reduce((hash, letter) => ((hash + letter.charCodeAt(0)) * 17) % 256, 0);
 }
 
-export class Day15 {
+function excute(boxes: Lens[][], sequence: string) {
+  const [label, focal] = sequence.replace("-", "").split("=");
+  const box = (boxes[hash(label)] ??= []);
+  const indexLens = box.findIndex((lens) => lens.label === label);
+  if (focal) {
+    if (indexLens != -1) {
+      box[indexLens].focal = +focal;
+    } else {
+      box.push(new Lens(label, +focal));
+    }
+  } else if (indexLens != -1) {
+    box.splice(indexLens, 1);
+  }
+}
+
+export class Day15Part1 {
   static solve(input: string): number {
     return input
       .trim()
       .split(",")
-      .map((val) => hash(val))
-      .reduce((sum, val) => sum + val, 0);
+      .reduce((sum, line) => sum + hash(line), 0);
+  }
+}
+
+export class Day15Part2 {
+  static solve(input: string): number {
+    const boxes: Lens[][] = [];
+    input
+      .replace(/[\r\n]+/, "")
+      .trim()
+      .split(",")
+      .forEach((sequence) => excute(boxes, sequence));
+    return boxes.reduce(
+      (sumPower, box, boxIdx) =>
+        sumPower +
+        box.reduce(
+          (power, { focal }, lensIndex) =>
+            power + (boxIdx + 1) * (lensIndex + 1) * focal,
+          0
+        ),
+      0
+    );
   }
 }

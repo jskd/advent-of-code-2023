c23cd9800759798723fa0bef6984328a22ee16e8
Jerome Skoda <contact@jeromeskoda.fr>
2023-12-19 17:25:38 +0100

    day 14 part 2

--- src/day-14/day-14.spec.ts
+++ src/day-14/day-14.spec.ts
@@ -1,7 +1,25 @@
-import { Day14Part1 } from "./day-14";
+import { Day14Part1, Day14Part2, NodeType, tiltLine } from "./day-14";
 import fs from "fs";
 
 describe("Day 14", () => {
+  it("tiltLine", () => {
+    expect(
+      tiltLine(
+        ".OO...#.O.".split("").map((v) => v as NodeType),
+        false
+      )
+    ).toEqual("OO....#O..".split("").map((v) => v as NodeType));
+  });
+
+  it("tiltLine reverse", () => {
+    expect(
+      tiltLine(
+        ".OO...#.O.".split("").map((v) => v as NodeType),
+        true
+      )
+    ).toEqual("....OO#..O".split("").map((v) => v as NodeType));
+  });
+
   const example =
     "O....#....\r\n" +
     "O.OO#....#\r\n" +
@@ -12,17 +30,20 @@ describe("Day 14", () => {
     "..O..#O..O\r\n" +
     ".......O..\r\n" +
     "#....###..\r\n" +
-    "#OO..#....";
+    "#OO..#....\r\n";
   it("Solve day 14 part 1 exemple", () => {
     expect(Day14Part1.solve(example)).toBe(136);
   });
-
-  /*it("Solve day 14 part 2 exemple", () => {
+  it("Solve day 14 part 2 exemple", () => {
     expect(Day14Part2.solve(example)).toBe(64);
-  });*/
+  });
 
-  /*const puzzle = fs.readFileSync(`${__dirname}/day-14-input.txt`).toString();
-  it("Solve day 14 part 1 puzzle", () => {
+  const puzzle = fs.readFileSync(`${__dirname}/day-14-input.txt`).toString();
+  it("Solve day 14 part 2 puzzle", () => {
     expect(Day14Part1.solve(puzzle)).toBe(109638);
-  });*/
+  });
+
+  it("Solve day 14 part 2 puzzle", () => {
+    expect(Day14Part2.solve(puzzle)).toBe(102657);
+  });
 });
--- src/day-14/day-14.ts
+++ src/day-14/day-14.ts
@@ -1,21 +1,22 @@
 import memoizee from "memoizee";
 
-type NodeType = "#" | "O" | ".";
+export type NodeType = "#" | "O" | ".";
+type Direction = "north" | "east" | "west" | "south";
 
-const tiltLine = memoizee(
-  (nodes: NodeType[], reverse = false) => {
+export const tiltLine = memoizee(
+  (nodes: NodeType[], reverse: boolean) => {
     const result: NodeType[] = Array<NodeType>(nodes.length).fill(".");
-    const incrementation = reverse ? -1 : 1;
-    let nextPlacement = reverse ? nodes.length - 1 : 0;
-    nodes.forEach((node, idx) => {
-      if (node === "#") {
-        result[idx] = "#";
-        nextPlacement = idx + incrementation;
-      } else if (node === "O") {
-        result[nextPlacement] = "O";
-        nextPlacement += incrementation;
+    const increment = reverse ? -1 : 1;
+    let start = reverse ? nodes.length - 1 : 0;
+    for (let i = start; i > -1 && i < nodes.length; i += increment) {
+      if (nodes[i] === "#") {
+        result[i] = "#";
+        start = i + increment;
+      } else if (nodes[i] === "O") {
+        result[start] = "O";
+        start += increment;
       }
-    });
+    }
     return result;
   },
   {
@@ -33,36 +34,72 @@ function turnCounterClockwise(nodes: NodeType[][]) {
   return nodes[0].map((_, index) => nodes.map((row) => row[index]).reverse());
 }
 
-function getSumLoad(nodes: NodeType[][]) {
-  let matrix = turnClockwise(nodes).map((l) => tiltLine(l));
-  matrix = turnCounterClockwise(matrix);
+function tilt(nodes: NodeType[][], direction: Direction) {
+  const clockTurn = direction === "north" || direction === "south";
+  const reverse = direction === "south" || direction === "east";
+  nodes = clockTurn ? turnClockwise(nodes) : nodes;
+  nodes = nodes.map((line) => tiltLine(line, reverse));
+  return clockTurn ? turnCounterClockwise(nodes) : nodes;
+}
 
-  return matrix.reduce(
+function getSumLoad(nodes: NodeType[][]) {
+  return nodes.reduce(
     (sum, line, index) =>
       line.reduce(
-        (sum, node) => sum + (node === "O" ? matrix.length - index : 0),
+        (sum, node) => sum + (node === "O" ? nodes.length - index : 0),
         sum
       ),
     0
   );
 }
 
+function hash(nodes: NodeType[][]) {
+  return nodes.flat().join("");
+}
+
+function tiltCycle(nodes: NodeType[][]) {
+  const currentpouet = new Map<string, number>();
+  const maxCycles = 1000000000;
+  for (let i = 0; i < maxCycles; i++) {
+    nodes = tilt(nodes, "north");
+    nodes = tilt(nodes, "west");
+    nodes = tilt(nodes, "south");
+    nodes = tilt(nodes, "east");
+
+    const loop = currentpouet.get(hash(nodes));
+    if (!loop) {
+      currentpouet.set(hash(nodes), i);
+    } else {
+      const remainingCycle = (maxCycles - 1 - i) % (i - loop);
+      for (let i = 0; i < remainingCycle; i++) {
+        nodes = tilt(nodes, "north");
+        nodes = tilt(nodes, "west");
+        nodes = tilt(nodes, "south");
+        nodes = tilt(nodes, "east");
+      }
+      return nodes;
+    }
+  }
+  return nodes;
+}
+
 export class Day14Part1 {
   static solve(input: string): number {
     const nodes = input
       .split(/[\r\n]+/)
+      .filter(Boolean)
       .map((line) => line.split("").map((value) => value as NodeType));
-    return getSumLoad(nodes);
+    return getSumLoad(tilt(nodes, "north"));
   }
 }
 
-/*export class Day14Part2 {
+export class Day14Part2 {
   static solve(input: string): number {
     const nodes = input
       .split(/[\r\n]+/)
-      .flatMap((line, x) =>
-        line.split("").map((value, y) => new Node(x, y, value as NodeType))
-      );
-    return new Grid(nodes).getSumLoadPart2();
+      .filter(Boolean)
+      .map((line) => line.split("").map((value) => value as NodeType));
+
+    return getSumLoad(tiltCycle(nodes));
   }
-}*/
+}
